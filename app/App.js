4684a37338b064921bd75ab9ad245f0864d2912a
maxmcd <max.t.mcdonnell@gmail.com>
2017-11-06 21:41:03 -0500

    Further exploration of ast and rendering

--- app/App.js
+++ app/App.js
@@ -1,35 +1,54 @@
-import React from 'react';
-import Expo from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Expo from "expo";
+import { StyleSheet, Text, View } from "react-native";
 const recast = require("recast");
-
-// console.log(esprima)
-
-// console.log(ast)
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {code: "hiasdf"}
-    Expo.FileSystem.downloadAsync("http://localhost:7654/App.js", `${Expo.FileSystem.documentDirectory}App.js`)
-    .then(({uri}) => {
-      console.log(uri)
-      return uri
-    })
-    .then(Expo.FileSystem.readAsStringAsync)
-    .then((body) => {
-      const ast = recast.parse(body)
-      const output = recast.prettyPrint(ast).code;
-      this.setState({code: output})
-    }).catch((err) => {
-      console.log(err)
-    })
+    super(props);
+    this.state = {
+      text: "",
+      ast: null,
+    };
+    Expo.FileSystem
+      .downloadAsync(
+        "http://localhost:7654/App.js",
+        `${Expo.FileSystem.documentDirectory}App.js`
+      )
+      .then(({ uri }) => {
+        console.log(uri);
+        return uri;
+      })
+      .then(Expo.FileSystem.readAsStringAsync)
+      .then(body => {
+        const ast = recast.parse(body);
+        // take every node of the ast
+        // wrap it a text and consider assigning
+        // it a color. then you have syntax highlighting
+        // based on object type
+        // then make each one clickable so that you
+        // can explore the data in each element
+        // be sure to format correctly and ensure that
+        // the performance of 1000's of Text elements
+        // is not terrible
+        const output = recast.prettyPrint(ast).code;
+        this.setState({
+          text: output.split('\n'),
+          ast: ast
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   renderText(err, data) {
     if (err) {
-      return console.log(err)
+      return console.log(err);
     }
-    this.setState({code: data})
+    this.setState({ code: data });
+  }
+  getTextAtLocation(location) {
+    this.state.text[location]
   }
   render() {
     return (
@@ -43,8 +62,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- app/experiments/basic-formatting.js
+++ app/experiments/basic-formatting.js
@@ -1,14 +1,97 @@
+require('colors')
+const assert = require("assert");
 const recast = require("recast");
-const esprima = require('esprima')
 
-const program = "const h = 'hi';()=>{console.log('foo')}"
+const program = "const h = 'hi';()=>{console.log('foo')}";
 
-const ast = recast.parse(program)
-const output = recast.prettyPrint(ast).code;
-console.log(output)
+let formatted_program = `const h = "hi";
+() => {
+    console.log("foo");
+};`;
 
-// const ast = esprima.parse(program, {sourceType: 'module', jsx: true})
-// console.log(ast)
-// console.log(prettier.__debug.formatAST(ast))
+const colors = [
+    "red",
+    "green",
+    "yellow",
+    "blue",
+    "magenta",
+    "cyan",
+    "white",
+    "gray",
+]
+
+const ast = recast.parse(formatted_program);
+
+let lines = formatted_program.split("\n");
+let out = ""
+let code_for_loc = (loc, color) => {
+    let relevant_lines = lines.slice(loc.start.line - 1, loc.end.line);
+
+    if (relevant_lines.length == 0) {
+        return
+    }
+    let lastIndex = relevant_lines.length - 1;
+    relevant_lines[lastIndex] = relevant_lines[lastIndex].substring(
+        0,
+        loc.end.column
+    );
+
+    relevant_lines[0] = relevant_lines[0].substring(
+        loc.start.column,
+        relevant_lines[0].length
+    );
+    return relevant_lines.join('\n')
+};
 
 
+let cursor = {
+    line: 0,
+    column: 0,
+    node: {type: "none", color: "black"},
+}
+let stack = []
+recast.visit(ast, {
+    visitNode: function(path) {
+        let color = colors[Math.floor(Math.random()*colors.length)];
+
+        path.node.color = color
+
+        new_cursor = {
+            line: path.node.loc.start.line,
+            column: path.node.loc.start.column,
+            node: path.node
+        }
+        process.stdout.write(path.node.type[color] + " \n")
+        process.stdout.write(code_for_loc(path.node.loc)[color])
+        process.stdout.write("\n")
+        stack.push(path.node)
+        // process.stdout.write(` str ${cursor.node.type} `[cursor.node.color])
+        // code_for_loc({start: cursor, end: new_cursor}, cursor.node.color)
+        cursor = new_cursor
+
+        this.traverse(path);
+
+        process.stdout.write(path.node.type[color] + " \n")
+        process.stdout.write(code_for_loc(path.node.loc)[color])
+        process.stdout.write("\n")
+
+        let child = stack.pop()
+        // console.log(child.type, path.node.type)
+        new_cursor = {
+            line: path.node.loc.end.line,
+            column: path.node.loc.end.column,
+            node: path.node
+        }
+        // code_for_loc({start: cursor, end: new_cursor}, color)
+        // process.stdout.write(` end ${new_cursor.node.type} `[color])
+        cursor = new_cursor
+    }
+});
+
+console.log(out)
+let printLevel = function() {};
+// console.log(ast);
+// console.log(JSON.stringify(ast, null, 4));
+
+// const output = recast.prettyPrint(ast).code;
+// console.log(output)
